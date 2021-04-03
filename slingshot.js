12756b4eaa2cdf509b29f2a31da5e19dba8bf2a8
HUMANGASORTHOR <umangchoraria6@gmail.com>
2021-04-03 12:12:17 +0530

    added slingshot.js

---
+++ slingshot.js
@@ -0,0 +1,17 @@
+class Slingshot {
+    constructor( bodyA,bodyB){
+  var options={
+    bodyA:bodyA,
+    bodyB:bodyB,
+    stiffness:0.04,
+    length:10
+ }
+  this.slingshot=Constraint.create(options);
+  World.add(world,this.slingshot);
+ }
+ display(){
+  var posA=this.slingshot.bodyA.position;
+  var posB=this.slingshot.bodyB.position; 
+   line (posA.x,posA.y,posB.x,posB.y);
+ }
+}
